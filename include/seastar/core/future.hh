23413965b4eea888e0c27274e46126817af37e95
Antoni Żewierżejew <azewierzejew@gmail.com>
2021-04-07 08:32:06 +0200

    Removed traces in get

--- include/seastar/core/future.hh
+++ include/seastar/core/future.hh
@@ -1463,7 +1463,6 @@ public:
     [[gnu::always_inline]]
     value_type&& get() {
         wait();
-        deadlock_detection::trace_edge(this, deadlock_detection::get_current_traced_ptr());
         return get_available_state_ref().take();
     }
 
@@ -1482,7 +1481,6 @@ public:
     /// Equivalent to: \c std::get<0>(f.get()).
     using get0_return_type = typename future_state::get0_return_type;
     get0_return_type get0() {
-        deadlock_detection::trace_edge(this, deadlock_detection::get_current_traced_ptr());
 #if SEASTAR_API_LEVEL < 5
         return future_state::get0(get());
 #else
